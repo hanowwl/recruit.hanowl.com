d9f3c74effed51c48d1de7ba57a9b715fcaab83f
HanSu Lee <i.hansu.lee@gmail.com>
2023-03-03 10:54:45 +0900

    feat: add supabase client

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly MODE: 'production' | 'development';
+
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
