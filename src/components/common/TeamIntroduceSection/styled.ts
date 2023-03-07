9ddc880480dba7f6c492a57a3d4f33934f5d1e1f
HanSu Lee <i.hansu.lee@gmail.com>
2023-03-07 10:41:10 +0900

    refactor(components): convert `px` unit to `rem` in `TeamIntroduceSection` component

--- src/components/common/TeamIntroduceSection/styled.ts
+++ src/components/common/TeamIntroduceSection/styled.ts
@@ -4,35 +4,32 @@ export const TeamIntroduceContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  padding: 30px 0;
+  padding: 3rem 0;
 
   & > section:not(:last-child) {
-    margin: 30px 0;
+    margin-bottom: 2.8rem;
   }
 `;
 
 export const SectionTitle = styled.h2`
-  font-size: 24px;
-  font-weight: 600;
+  font-size: 2rem;
+  font-weight: 700;
   margin-bottom: 20px;
 `;
 
 export const SectionDescription = styled.p`
-  font-size: 16px;
   font-weight: 400;
-  width: 60%;
   line-height: 1.5;
   color: #bbbbbb;
 `;
 
 export const SectionListUl = styled.ul`
-  margin-left: 20px;
+  margin-left: 2rem;
   list-style: inside !important;
 `;
 
 export const SectionListItem = styled.li`
-  font-size: 16px;
-  font-weight: 400;
   color: #bbbbbb;
+  font-weight: 400;
   line-height: 1.5;
 `;
