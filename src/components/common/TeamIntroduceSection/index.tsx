e255a3e92e2684da07684dbb8abd5891e50988d0
HanSu Lee <i.hansu.lee@gmail.com>
2023-03-07 10:24:48 +0900

    fix(components): add `key` prop into TeamIntroductionSection component

--- src/components/common/TeamIntroduceSection/index.tsx
+++ src/components/common/TeamIntroduceSection/index.tsx
@@ -13,7 +13,7 @@ export const TeamIntroduceSection: React.FC<TeamIntroduceSectionProps> = ({ sect
     <S.TeamIntroduceContainer>
       {sections.map(({ title, text, list }) => {
         return (
-          <section>
+          <section key={title}>
             <S.SectionTitle>{title}</S.SectionTitle>
 
             {text && <S.SectionDescription>{text}</S.SectionDescription>}
