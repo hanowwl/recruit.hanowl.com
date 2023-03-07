a9c77efd279597c08dabe489b70e2ac13fe1d89f
HanSu Lee <i.hansu.lee@gmail.com>
2023-03-07 11:12:13 +0900

    fix(constant): modify `TeamSection.text` type to `React.ReactNode`

--- src/constant/teams.tsx
+++ src/constant/teams.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export interface TeamTextSection {
   title: string;
   text: string;
@@ -10,7 +12,7 @@ export interface TeamListSection {
 
 export interface TeamSection {
   title: string;
-  text?: string;
+  text?: React.ReactNode;
   list?: string[];
 }
 
@@ -29,10 +31,19 @@ export const TEAM_LIST: Team[] = [
     sections: [
       {
         title: '기능부 소개',
-        text: `학생회 기능부는 주로 교내에서 사용할 서비스를 만들어요. 매년 운영되는 한세톤(해커톤),
-        한세어울림마당(축제) 등에서 사용될 서비스를 만들고 배포/운영까지 담당해요. 여러
-        프로젝트에 참여하면서 기획/디자인/개발/운영 모든 단계에 참여하면서, 경험을 쌓을 수
-        있어요. 그리고 기능부는 서로 지식을 공유하고 자극이 되는 관계를 지향해요.`,
+        text: (
+          <>
+            학생회 기능부는 주로 교내에서 사용할 서비스를 만들어요.
+            <br />
+            매년 운영되는 한세톤(해커톤), 한세어울림마당(축제) 등에서 사용될 서비스를 만들고
+            배포/운영까지 담당해요.
+            <br />
+            여러 프로젝트에 참여하면서 기획/디자인/개발/운영 모든 단계에 참여하면서, 경험을 쌓을 수
+            있어요.
+            <br />
+            그리고 기능부는 서로 지식을 공유하고 자극이 되는 관계를 지향해요.
+          </>
+        ),
       },
       {
         title: '인재상',
