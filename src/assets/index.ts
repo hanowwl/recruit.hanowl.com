1de68474061232f3f0969fdb1911731de663de79
HanSu Lee <i.hansu.lee@gmail.com>
2023-03-12 01:09:22 +0900

    feat(pages): add `MainSection` of main page

---
+++ src/assets/index.ts
@@ -0,0 +1,2 @@
+export { default as Banner1PNG } from './banner-1.png';
+export { default as Banner2PNG } from './banner-2.png';
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,40 +1,7 @@
 import React from 'react';
 
-import { BlurCircle } from '@/components';
+import { MainSection } from '@/components/main';
 
 export const MainPage: React.FC = () => {
-  return (
-    <div>
-      <BlurCircle.Section
-        circles={[
-          {
-            width: 110,
-            height: 50,
-            animate: {
-              x: [230, -30, 230],
-              y: [0, -100, -80, 0],
-              backgroundColor: [
-                'rgba(87, 60, 255, 0.07)',
-                'rgba(87, 60, 255, 0.1)',
-                'rgba(87, 60, 255, 0.07)',
-              ],
-            },
-          },
-          {
-            width: 80,
-            height: 40,
-            animate: {
-              x: [-300, 0, -300],
-              y: [300, 200, 300],
-              backgroundColor: [
-                'rgba(177, 41, 241, 0.07)',
-                'rgba(177, 41, 241, 0.1)',
-                'rgba(177, 41, 241, 0.07)',
-              ],
-            },
-          },
-        ]}
-      ></BlurCircle.Section>
-    </div>
-  );
+  return <MainSection />;
 };
