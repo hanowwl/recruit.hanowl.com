62ee12444081d1b0f2e5eb1f6da53344983d4992
HanSu Lee <i.hansu.lee@gmail.com>
2023-03-13 01:32:34 +0900

    feat(constants): modify href of `MENU_LIST`

--- src/constant/menu.ts
+++ src/constant/menu.ts
@@ -3,14 +3,14 @@ import { NavbarMenuItem } from '../components';
 export const MENU_LIST: NavbarMenuItem[] = [
   {
     text: '모집공고',
-    href: '',
+    href: '/teams',
   },
   {
     text: '자주 묻는 질문',
-    href: '',
+    href: '/qna',
   },
   {
     text: '로그인',
-    href: '',
+    href: '/auth/signin',
   },
 ];
