d71a2ee79ef3b867a3f678b4a10b9d77552b3375
최근원 <kidjustinchoi@gmail.com>
2023-03-13 02:15:22 +0900

    feat: support qna mobile style

--- src/pages/qna/index.tsx
+++ src/pages/qna/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import { useLocation } from 'react-router-dom';
 
@@ -10,31 +10,77 @@ import * as S from './styled';
 
 export const QnAPage: React.FC = () => {
   const [isOpened, setIsOpened] = useState<boolean>(false);
+  const location = useLocation();
 
   const onClick = () => {
     setIsOpened(!isOpened);
   };
 
-  const location = useLocation();
+  const checkActive = (href: string) => {
+    return location.pathname === `/qna/${href}`;
+  };
+
+  const [windowDimension, detectHW] = useState({ winWidth: window.innerWidth });
+
+  const detectSize = () => {
+    detectHW({
+      winWidth: window.innerWidth,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', detectSize);
+
+    return () => {
+      window.removeEventListener('resize', detectSize);
+    };
+  }, [windowDimension]);
 
   return (
     <S.QnAContainer>
       <S.Title>자주 묻는 질문</S.Title>
-      <S.Menu
-        onClick={() => {
-          onClick();
-        }}
-      >
-        {QNA_MENU_LIST.filter(({ href }) => location.pathname === `/qna/${href}`)[0].text}
-        {isOpened ? <FaChevronUp /> : <FaChevronDown />}
-      </S.Menu>
-      {isOpened && (
-        <SelectMenuSection onClick={onClick} qnaMenuList={QNA_MENU_LIST} location={location} />
-      )}
-      {QNA_MENU_LIST.filter(({ href }) => location.pathname === `/qna/${href}`)[0].list.map(
-        ({ question, answer }, index) => (
-          <QnASection key={index} question={question} answer={answer} />
-        )
+      {windowDimension.winWidth <= 767 ? (
+        <>
+          <S.Menu
+            onClick={() => {
+              onClick();
+            }}
+          >
+            {QNA_MENU_LIST.filter(({ href }) => checkActive(href))[0].text}
+            {isOpened ? <FaChevronUp /> : <FaChevronDown />}
+          </S.Menu>
+          {isOpened && (
+            <SelectMenuSection onClick={onClick} qnaMenuList={QNA_MENU_LIST} location={location} />
+          )}
+          {QNA_MENU_LIST.filter(({ href }) => checkActive(href))[0].list.map(
+            ({ question, answer }, index) => (
+              <QnASection key={index} question={question} answer={answer} />
+            )
+          )}
+        </>
+      ) : (
+        <S.QnAContentContainer>
+          <S.Menu
+            onClick={() => {
+              onClick();
+            }}
+          >
+            {QNA_MENU_LIST.map(({ text, href }, index) => {
+              return (
+                <S.QnAMenuContent key={index} isActive={checkActive(href)} to={`/qna/${href}`}>
+                  {text}
+                </S.QnAMenuContent>
+              );
+            })}
+          </S.Menu>
+          <S.QnASectionWrapper>
+            {QNA_MENU_LIST.filter(({ href }) => checkActive(href))[0].list.map(
+              ({ question, answer }, index) => (
+                <QnASection key={index} question={question} answer={answer} />
+              )
+            )}
+          </S.QnASectionWrapper>
+        </S.QnAContentContainer>
       )}
     </S.QnAContainer>
   );
--- src/pages/qna/styled.ts
+++ src/pages/qna/styled.ts
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import styled from '@emotion/styled';
 
 export const QnAContainer = styled.div`
@@ -16,14 +18,37 @@ export const Menu = styled.button`
   border: none;
   padding: 2rem;
   display: flex;
-  align-items: center;
   justify-content: space-between;
   border-radius: 2rem;
   background-color: rgb(241, 243, 245);
   font-size: 1.8rem;
   font-weight: 700;
   margin-bottom: 3rem;
+  align-items: flex-start;
   color: rgb(52, 58, 64);
+  @media screen and (min-width: 767px) {
+    flex-direction: column;
+    background-color: transparent;
+  }
 `;
 
-// export const MenuIcon
+export const QnAMenuContent = styled(Link)<{ isActive: boolean }>`
+  font-weight: 800;
+  font-size: 2rem;
+  line-height: normal;
+  padding: 1.4rem;
+  text-decoration: none;
+  color: ${({ isActive }) => (isActive ? 'white !important' : '#ADB5BD !important')};
+`;
+
+export const QnAContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const QnASectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+`;
