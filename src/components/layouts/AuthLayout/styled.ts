386fdbda289f10e2496759592fc19c63f98fbcab
HanSu Lee <i.hansu.lee@gmail.com>
2023-03-13 11:57:46 +0900

    fix(components): replace `height` style to `min-height` in AuthLayout

--- src/components/layouts/AuthLayout/styled.ts
+++ src/components/layouts/AuthLayout/styled.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const AuthLayoutContainer = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 100%;
 `;
 
 export const AuthLayoutWrapper = styled.main`
