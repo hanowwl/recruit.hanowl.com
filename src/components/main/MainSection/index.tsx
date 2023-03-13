7090745975cecf7215c964c30bffb0edea1d3a05
HanSu Lee <i.hansu.lee@gmail.com>
2023-03-13 14:16:52 +0900

    fix(components): disable MainSection cricles for mobile support

--- src/components/main/MainSection/index.tsx
+++ src/components/main/MainSection/index.tsx
@@ -9,34 +9,36 @@ export const MainSection: React.FC = () => {
 
   return (
     <BlurCircle.Section
-      circles={[
-        {
-          width: 110,
-          height: 50,
-          animate: {
-            x: [230, -30, 230],
-            y: [0, -100, -80, 0],
-            backgroundColor: [
-              'rgba(87, 60, 255, 0.07)',
-              'rgba(87, 60, 255, 0.1)',
-              'rgba(87, 60, 255, 0.07)',
-            ],
-          },
-        },
-        {
-          width: 80,
-          height: 40,
-          animate: {
-            x: [-300, 0, -300],
-            y: [300, 200, 300],
-            backgroundColor: [
-              'rgba(177, 41, 241, 0.07)',
-              'rgba(177, 41, 241, 0.1)',
-              'rgba(177, 41, 241, 0.07)',
-            ],
-          },
-        },
-      ]}
+      circles={
+        [
+          // {
+          //   width: 110,
+          //   height: 50,
+          //   animate: {
+          //     x: [230, -30, 230],
+          //     y: [0, -100, -80, 0],
+          //     backgroundColor: [
+          //       'rgba(87, 60, 255, 0.07)',
+          //       'rgba(87, 60, 255, 0.1)',
+          //       'rgba(87, 60, 255, 0.07)',
+          //     ],
+          //   },
+          // },
+          // {
+          //   width: 80,
+          //   height: 40,
+          //   animate: {
+          //     x: [-300, 0, -300],
+          //     y: [300, 200, 300],
+          //     backgroundColor: [
+          //       'rgba(177, 41, 241, 0.07)',
+          //       'rgba(177, 41, 241, 0.1)',
+          //       'rgba(177, 41, 241, 0.07)',
+          //     ],
+          //   },
+          // },
+        ]
+      }
     >
       <Text.Column {...animation}>
         <Text.Row>
